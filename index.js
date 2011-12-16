51aab1bf66ebd7df327d9f1ed97a233e1af3fe9d
Tom Blobaum <tblobaum@gmail.com>
2011-12-15 23:25:34 -0600

    fixing bug

--- index.js
+++ index.js
@@ -5,24 +5,23 @@
  * MIT Licensed
  */
 
-var subs = []
+var subs = [{base:'*'}]
 
 module.exports = {
   use: function (base, options) {
     options = options || {}
-    subs.push({
+    subs.unshift({
       base:base, 
       options:options
     })
   },
   middleware: function (req, res, next) {
     subs.forEach(function(subdomain) {
-      if (subdomain.base.test(req.headers.host)) {
+      if (RegExp(subdomain.base, "gi").test(req.headers.host)) {
         req.url = '/' + subdomain.base + req.url
         return next()
       }
     })
-    return next()
   }
 }
 
