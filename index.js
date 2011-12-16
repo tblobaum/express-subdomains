010e8e5e805de565c9927d61d56eedda6a541901
Tom Blobaum <tblobaum@gmail.com>
2011-12-15 23:54:48 -0600

    updated with async foreach

--- index.js
+++ index.js
@@ -6,7 +6,8 @@
  */
 
 var subs = [{base:'.'}]
-
+var Sequence = require('sequence');
+  
 module.exports = {
   use: function (base, options) {
     options = options || {}
@@ -14,14 +15,37 @@ module.exports = {
       base:base, 
       options:options
     })
+    return this
   },
   middleware: function (req, res, next) {
-    subs.forEach(function(subdomain) {
-      if (RegExp(subdomain.base, "gi").test(req.headers.host)) {
-        req.url = '/' + subdomain.base + req.url
-        return next()
+  
+    forEachAsync(subs, function(fn, item, i, arr) {
+    
+      if (RegExp(item.base, "gi").test(req.headers.host)) {
+        if (item.base !== '.') req.url = '/' + item.base + req.url
+        next()
       }
+      
+      else {
+        fn()
+      }
+      
+    })
+    
+  }
+}
+
+function forEachAsync (arr, callback) {
+  var sequence = Sequence()
+
+  function handleItem(item, i, arr) {
+    sequence.then(function (next) {
+      callback(next, item, i, arr)
     })
   }
+
+  arr.forEach(handleItem)
+
+  return sequence
 }
 
