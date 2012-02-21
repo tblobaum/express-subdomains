bb06b272f6f1f7bdae8a7bc8eb0ac076d97b31d5
tblobaum <tblobaum@gmail.com>
2012-02-20 19:25:02 -0600

    add test case for subdomain and route combination

--- test/subdomains.test.js
+++ test/subdomains.test.js
@@ -4,14 +4,14 @@ var assert = require('assert')
   , subdomains = require(__dirname +'/../lib/subdomains')
 
 // Run tests
-describe('subdomains.use', function () {
+describe('express-subdomains', function () {
 
   it('should convert a basic subdomain into a route', function (done) {
     var args = [
       { url: '/', headers: { host: 'api.domain.com' } },
       {},
       function (req, res, next) { 
-        assert.strictEqual(req.url, '/api/', 'route should be `/api/`')
+        assert.strictEqual(req.url, '/api', 'route should be `/api`')
         done()
       }
     ]
@@ -24,11 +24,24 @@ describe('subdomains.use', function () {
       { url: '/', headers: { host: 'api.01.vanity.domain.com' } },
       {},
       function (req, res, next) { 
-        assert.strictEqual(req.url, '/api.01.vanity/', 'route should be `/api.01.vanity/`')
+        assert.strictEqual(req.url, '/api.01.vanity', 'route should be `/api.01.vanity`')
         done()
       }
     ]
     subdomains.use('api.01.vanity')
+    subdomains.middleware.apply({}, args)
+  })
+  
+  it('should convert a subdomain into a route and combine the path', function (done) {
+    var args = [
+      { url: '/users', headers: { host: 'api.domain.com/users' } },
+      {},
+      function (req, res, next) {
+        assert.strictEqual(req.url, '/api/users', 'route should be `/api/users`')
+        done()
+      }
+    ]
+    subdomains.use('api')
     subdomains.middleware.apply({}, args)
   })
   
