e518313d42d617960bfd3b9cc7725f234d1a9145
Gabriel Montes <gabriel@iamat.com>
2013-03-05 14:40:57 -0300

    Added strict subdomain check, domain option and tests.

--- test/subdomains.test.js
+++ test/subdomains.test.js
@@ -7,10 +7,11 @@ var assert = require('assert')
 describe('express-subdomains', function () {
 
   it('should convert a basic subdomain into a route', function (done) {
-    var args = [
-      { url: '/', headers: { host: 'api.domain.com' } },
+    var req = { url: '/', headers: { host: 'api.domain.com' } },
+      args = [
+      req,
       {},
-      function (req, res, next) { 
+      function () {
         assert.strictEqual(req.url, '/api', 'route should be `/api`')
         done()
       }
@@ -18,12 +19,13 @@ describe('express-subdomains', function () {
     subdomains.use('api')
     subdomains.middleware.apply({}, args)
   })
-  
+
   it('should convert a multi level subdomain into a route', function (done) {
-    var args = [
-      { url: '/', headers: { host: 'api.01.vanity.domain.com' } },
+    var req = { url: '/', headers: { host: 'api.01.vanity.domain.com' } },
+      args = [
+      req,
       {},
-      function (req, res, next) { 
+      function () {
         assert.strictEqual(req.url, '/api.01.vanity', 'route should be `/api.01.vanity`')
         done()
       }
@@ -31,12 +33,13 @@ describe('express-subdomains', function () {
     subdomains.use('api.01.vanity')
     subdomains.middleware.apply({}, args)
   })
-  
+
   it('should convert a subdomain into a route and combine the path', function (done) {
-    var args = [
-      { url: '/users', headers: { host: 'api.domain.com/users' } },
+    var req = { url: '/users', headers: { host: 'api.domain.com/users' } },
+      args = [
+      req,
       {},
-      function (req, res, next) {
+      function () {
         assert.strictEqual(req.url, '/api/users', 'route should be `/api/users`')
         done()
       }
@@ -44,6 +47,61 @@ describe('express-subdomains', function () {
     subdomains.use('api')
     subdomains.middleware.apply({}, args)
   })
-  
-})
 
+  it('should not convert any start match that is not a subdomain into a route', function (done) {
+    var req = { url: '/', headers: { host: 'apinew.domain.com' } },
+      args = [
+      req,
+      {},
+      function () {
+        assert.strictEqual(req.url, '/', 'route should be `/`')
+        done()
+      }
+    ]
+    subdomains.use('api')
+    subdomains.middleware.apply({}, args)
+  })
+
+  it('should not convert any partial match into a route', function (done) {
+    var req = { url: '/', headers: { host: 'newapi.domain.com' } },
+      args = [
+      req,
+      {},
+      function () {
+        assert.strictEqual(req.url, '/', 'route should be `/`')
+        done()
+      }
+    ]
+    subdomains.use('api')
+    subdomains.middleware.apply({}, args)
+  })
+
+  it('should convert a subdomain and domain into a route', function (done) {
+    var req = { url: '/', headers: { host: 'api.domain.com' } },
+      args = [
+      req,
+      {},
+      function () {
+        assert.strictEqual(req.url, '/api', 'route should be `/api`')
+        done()
+      }
+    ]
+    subdomains.domain('domain.com').use('api')
+    subdomains.middleware.apply({}, args)
+  })
+
+  it('should not convert a subdomain with different domain into a route', function (done) {
+    var req = { url: '/', headers: { host: 'api.otherdomain.com' } },
+      args = [
+      req,
+      {},
+      function () {
+        assert.strictEqual(req.url, '/', 'route should be `/`')
+        done()
+      }
+    ]
+    subdomains.domain('domain.com').use('api')
+    subdomains.middleware.apply({}, args)
+  })
+
+})
